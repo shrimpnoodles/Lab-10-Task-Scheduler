9f38463c2be8b7c9d0a1f8ab084db425b3b79761
shrimpnoodles <77302524+shrimpnoodles@users.noreply.github.com>
2021-02-21 19:37:33 -0800

    Update hcier001_lab10_part1.c

--- turnin/hcier001_lab10_part1.c
+++ turnin/hcier001_lab10_part1.c
@@ -3,7 +3,7 @@
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
- *
+ *	DEMO LINK: https://drive.google.com/file/d/1oMAWPddmbIWT0PijTr7S9SsRvz6PFrzC/view?usp=sharing
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
